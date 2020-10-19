92933cee7ccc93c72fd3e3731b74a5e7970f2ce6
unknown <cannicam@gmail.com>
2020-10-19 14:29:07 -0500

    new prog

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Transactions;
+
+namespace Intro_exer_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, verb, adj, adj_a;
+
+            Console.Write("Enter a Color:");
+            color = Console.ReadLine();
+            Console.Write("Enter a Verb:");
+            verb = Console.ReadLine();
+            Console.Write("Enter a Adjective:");
+            adj = Console.ReadLine();
+            Console.Write("Enter another Adjective:");
+            adj_a = Console.ReadLine();
+
+            Console.WriteLine("The " + color + " cat");
+            Console.WriteLine(verb + " at the old man");
+            Console.WriteLine("The old man was " + adj);
+            Console.WriteLine("But the Cat was " + adj_a);
+
+            Console.ReadLine();
+        }
+    }
+}
